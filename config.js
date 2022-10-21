be921538c12a7d6920a4a405ba54cf3afb7c9deb
BraianAvalos <69589196+BraianAvalos@users.noreply.github.com>
2022-10-20 18:04:54 -0600

    cambio de config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const configuration = {
     mongodb:{
-        url:process.env.DB_URL || "mongodb+srv://adminRoot:adminRoot@persona.blwchb3.mongodb.net/real-state"
+        url:process.env.DB_URL 
     }
 }
 
